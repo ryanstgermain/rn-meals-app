23f3efad44708ed66961e974240724000cf83664
ryanstgermain <ryanjoel26@gmail.com>
2020-04-23 17:33:50 -0600

    Added some styling.

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -9,13 +9,15 @@ import {
 } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data.js';
+import CategoryGridTile from '../components/CategoryGridTile.js';
 
 const CategoriesScreen = props => {
   const renderGridItem = (itemData) => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate({
             routeName: 'CategoryMeals',
             params: {
@@ -23,13 +25,7 @@ const CategoriesScreen = props => {
             }
           });
         }}
-      >
-        <View>
-          <Text>
-            {itemData.item.title}
-          </Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
 
@@ -52,12 +48,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150
   }
 });
 
